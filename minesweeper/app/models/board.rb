4b5375f9c71ba61242f1f8373676e1dbfba610d3
Bruno Montebello <brunormontebello@hotmail.com>
2024-01-10 13:18:48 -0300

    Add validations to board model

--- minesweeper/app/models/board.rb
+++ minesweeper/app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ApplicationRecord
+  validates :name, :email, :height, :width, :mines_number, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { maximum: 50 }
+  validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 end
