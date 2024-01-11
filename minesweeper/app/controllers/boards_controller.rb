49ec5de3be95cdba2d7111b4a650e12c73dec93c
Bruno Montebello <brunormontebello@hotmail.com>
2024-01-10 21:36:05 -0300

    feat: create boards_controller + set routes

---
+++ minesweeper/app/controllers/boards_controller.rb
@@ -0,0 +1,34 @@
+class BoardsController < ApplicationController
+  before_action :set_board, only: %i[show]
+
+  def index
+    @boards = Board.all
+  end
+
+  def show
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      redirect_to board_url @board, notice: 'Successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+
+  def board_params
+    params.require(:board).permit(:name, :email, :height, :width, :mines_number)
+  end
+end
--- minesweeper/config/routes.rb
+++ minesweeper/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :boards
   root "pages#home"
 end
