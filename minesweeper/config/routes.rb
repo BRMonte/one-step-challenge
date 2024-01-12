e6853ed6a039c8beb65e4a8299647361cd771d8e
Bruno Montebello <brunormontebello@hotmail.com>
2024-01-12 03:18:39 -0300

    refact: limit routes

--- minesweeper/config/routes.rb
+++ minesweeper/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :boards
+  resources :boards, only: %i[index show create]
   root "pages#home"
 end
