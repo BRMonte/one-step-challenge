fdb63fa4477ba6a48f8e14a35417227e8d47137a
Bruno Montebello <brunormontebello@hotmail.com>
2024-01-12 04:09:20 -0300

    feat: create service to generate board

---
+++ minesweeper/app/services/board_generator_service.rb
@@ -0,0 +1,33 @@
+class BoardGeneratorService
+  MINE = 'x'
+  EMPTY = 0
+
+  def initialize(board)
+    @height = board.height
+    @width = board.width
+    @mines_number = board.mines_number
+  end
+
+  def call
+    board = initialize_board
+    place_mines(board)
+    board
+  end
+
+  private
+
+  def initialize_board
+    Array.new(@height) { Array.new(@width, EMPTY) }
+  end
+
+  def place_mines(board)
+    @mines_number.times do
+      row = rand(@height)
+      col = rand(@width)
+
+      redo if board[row][col] == MINE
+
+      board[row][col] = MINE
+    end
+  end
+end
