2996c409e1f4ff0a2ff83fdd3e3b849aebbd8a0c
Bruno Montebello <brunormontebello@hotmail.com>
2024-01-14 14:06:12 -0300

    fix: board bug not placing mines at the right cell

--- minesweeper/app/helpers/boards_helper.rb
+++ minesweeper/app/helpers/boards_helper.rb
@@ -3,4 +3,8 @@ module BoardsHelper
   def get_board_technical_representation(board)
     BoardGeneratorService.new(board).call
   end
+
+  def get_cell_value(value)
+    value.nil? ? ' ' : 'X'
+  end
 end
